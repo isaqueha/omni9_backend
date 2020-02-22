a13814f6672c71ea413494c7062d936f58b32364
Isaque Azevedo <isaque.azevedo@sap.com>
2020-02-21 21:59:10 -0300

    feat: websocket booking approvation

--- src/router.js
+++ src/router.js
@@ -6,6 +6,8 @@ const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 const DashboardController = require('./controllers/DashboardController');
 const BookingController = require('./controllers/BookingController');
+const ApprovalController = require('./controllers/ApprovalController');
+const RejectionController = require('./controllers/RejectionController');
 
 const router = express.Router();
 const upload = multer(uploadConfig);
@@ -20,6 +22,9 @@ router.get('/spots', SpotController.index);
 router.post('/spots', upload.single('thumbnail'), SpotController.store);
 
 router.post('/spots/:spot_id/bookings', BookingController.store);
+
+router.post('/bookings/:booking_id/approvals', ApprovalController.store);
+router.post('/bookings/:booking_id/rejections', RejectionController.store);
 
 router.get('/dashboard', DashboardController.show);
 
--- src/server.js
+++ src/server.js
@@ -2,9 +2,18 @@ const express = require('express')
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+
+const socketio = require('socket.io');
+const http = require('http');
+
 const router = require('./router');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+const connectedUsers = {};
+
 
 // https://cloud.mongodb.com/v2/5d96469bff7a2577dd4000b4#clusters
 
@@ -13,9 +22,22 @@ mongoose.connect('mongodb+srv://famTree:famTree@famtree-y6fg2.mongodb.net/famTre
 	useUnifiedTopology: true
 });
 
+io.on('connection', socket => {
+	const { user_id } = socket.handshake.query;
+
+	connectedUsers[user_id] = socket.id;
+})
+
+app.use((req, res, next) => {
+	req.io = io;
+	req.connectedUsers = connectedUsers;
+
+	return next();
+})
+
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(router);
 
-app.listen(3333);
+server.listen(3333);
